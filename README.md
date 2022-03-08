d286e7ffefa22d8fdb9c153819301bba4bae7e26
Underthestars-zhy <zhuhaoyu0909@icloud.com>
2022-03-08 18:53:44 +0800

    first

--- README.md
+++ README.md
@@ -1,3 +1,43 @@
 # SwiftUIKeyPress
 
-A description of this package.
+SwiftUIKeyPress is a package to make up for the lack of keyboard input in SwiftUI.
+
+!()[SwiftUIKeyPress]
+
+## How to use?
+
+There are two ways that you can implement `SwiftUIPress`
+
+First:
+
+```swift
+struct ContentView: View
+    @State var keys = [UIKey]()
+
+    var body: some View {
+        Text(keys.map(\.characters).reduce("", +))
+            .padding()
+            .onKeyPress($keys)
+    }
+}
+```
+
+Second:
+
+```swift
+struct ContentView: View
+    @State var keys = [UIKey]()
+
+    var body: some View {
+        Text(keys.map(\.characters).reduce("", +))
+            .padding()
+            .onKeyPress { keys in
+                self.keys = keys
+            }
+    }
+}
+```
+
+## A Tips
+
+You only can use 1 `SwiftUIKeyPress` modifier on every one view. The others will not work.
--- Sources/SwiftUIKeyPress/SwiftUIKeyPress.swift
+++ Sources/SwiftUIKeyPress/SwiftUIKeyPress.swift
@@ -1,6 +1,93 @@
-public struct SwiftUIKeyPress {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
 
-    public init() {
+public extension View {
+    func onKeyPress(_ keys: Binding<[UIKey]>) -> some View {
+        self
+            .modifier(KeyPressModifier(keys: keys))
     }
+    
+    func onKeyPress(_ action: @escaping ([UIKey]) -> ()) -> some View {
+        self
+            .modifier(DoOnKeyPressModifier(action: action))
+    }
+}
+
+struct KeyPressModifier: ViewModifier {
+    @Binding var keys: [UIKey]
+    
+    func body(content: Content) -> some View {
+        content
+            .background(KeyPressView(keys: $keys))
+    }
+}
+
+struct DoOnKeyPressModifier: ViewModifier {
+    @State var keys: [UIKey] = []
+    
+    var action: ([UIKey]) -> ()
+    
+    func body(content: Content) -> some View {
+        content
+            .background(KeyPressView(keys: $keys))
+            .onChange(of: keys) { newValue in
+                action(newValue)
+            }
+    }
+}
+
+struct KeyPressView: UIViewControllerRepresentable {
+    typealias UIViewControllerType = KeyPressViewController
+    
+    class Coordinator: NSObject, KeyPressViewControllerDelegate {
+        var parent: KeyPressView
+        
+        init(_ parent: KeyPressView) {
+            self.parent = parent
+        }
+        
+        func tap(_ key: [UIKey]) {
+            self.parent.keys.append(contentsOf: key)
+        }
+    }
+    
+    @Binding var keys: [UIKey]
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIViewController(context: Context) -> KeyPressViewController {
+        let controller = KeyPressViewController()
+        controller.delegate = context.coordinator
+        return controller
+    }
+    
+    func updateUIViewController(_ uiViewController: KeyPressViewController, context: Context) {
+        
+    }
+}
+
+class KeyPressViewController: UIViewController {
+    var delegate: KeyPressViewControllerDelegate? = nil
+    
+    override func viewDidLoad() {
+        self.view.addSubview(UIHostingController(rootView: Color.clear.id(UUID())).view)
+    }
+    
+    override func pressesBegan(_ presses: Set<UIPress>, with event: UIPressesEvent?) {
+        var result = [UIKey]()
+        for press in presses {
+            guard let key = press.key else { continue }
+            
+            result.append(key)
+        }
+        
+        delegate?.tap(result)
+        
+        super.pressesBegan(presses, with: event)
+    }
+}
+
+protocol KeyPressViewControllerDelegate {
+    func tap(_ key: [UIKey])
 }
